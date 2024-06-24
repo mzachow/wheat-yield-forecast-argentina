4040acbbbbdc987c89da7aefc23a6d8a0f77ddf7
Maximilian Zachow <maxzachow@protonmail.com>
2024-06-24 09:30:11 +0200

    changes to review #2

--- modules/forecast.py
+++ modules/forecast.py
@@ -11,7 +11,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.feature_selection import SelectKBest, f_regression, VarianceThreshold
   
 # K-Fold Cross Validation
-def kfold_cross_validation(data, model="ECMWF", init=8, augment=False, no_of_features=3):
+def kfold_cross_validation(data, model="ECMWF", region="Chaco húmedo sur", init=8, feature_names=["tmean_8"], no_of_features="all"):
     """Retrain, select features, and directly forecast yield on national level.
     
     params:
@@ -25,41 +25,34 @@ def kfold_cross_validation(data, model="ECMWF", init=8, augment=False, no_of_fea
      - national_forecasts_by_year: dataframe with forecasted and observed national wheat yield for the selected model and month of initialization on cross validation
     """
     # Filter by model and init_month but also include observations that are used for model training
-    if init == 12: model = "ERA"
-    cv_dataset = (data.loc[((data["model"] == model) & (data["init_month"] == init))
-                               | ((data["model"] == "ERA") & (data["init_month"] == 12))])
-    # Dataframe where interim results are saved
-    national_forecasts_by_year = (pd.DataFrame(data={"year":list(range(1993,2017)), "predicted":np.zeros(24)})
-                                  .merge(cv_dataset.loc[(cv_dataset["model"] == "ERA"), 
-                                                        ["year", "yield anomaly [%]"]], on="year", how="left"))
-    # Features
-    relevant_columns = [c for c in cv_dataset.columns if c not in ["model", "init_month", "year", "yield [kg/ha]", "yield_trend [kg/ha]", "yield anomaly [%]"]]
-    
+    cv_dataset = data.loc[((data["model"] == model) & (data["init_month"] == init) & (data["region"] == region))
+                        | ((data["model"] == "ERA") & (data["init_month"] == 12) & (data["region"] == region))]
+    # Dataframe where predictions are saved
+    forecasts_by_year = cv_dataset.loc[(cv_dataset["model"] == "ERA"), ["year", "yield anomaly [%]", "yield_loss"]].assign(predicted=np.nan)  
+    # Dataframe where final results are saved
     year_to_features = {}
-    if augment: models_for_training = ["ERA", model]
-    else: models_for_training = ["ERA"]
+    
     for season in list(range(1993,2017)):
-        X_train = cv_dataset.loc[((cv_dataset["model"].isin(models_for_training)) & (cv_dataset["year"] != season)), relevant_columns].reset_index(drop=True)
-        y_train = cv_dataset.loc[((cv_dataset["model"].isin(models_for_training)) & (cv_dataset["year"] != season)), "yield anomaly [%]"].reset_index(drop=True)
+        X_train = cv_dataset.loc[((cv_dataset["model"] == "ERA") & (cv_dataset["year"] != season)), feature_names].reset_index(drop=True)
+        y_train = cv_dataset.loc[((cv_dataset["model"] == "ERA") & (cv_dataset["year"] != season)), "yield anomaly [%]"].reset_index(drop=True)
         
-        #print(season)
-        #print(X_train)    
         pipeline = Pipeline([('scaler', StandardScaler()), 
-                             ('var', VarianceThreshold()), 
                              ('selector', SelectKBest(f_regression, k=no_of_features)),
-                             ('estimator', Ridge())])
+                             ('estimator', Ridge(alpha=8))])
         
         reg = pipeline.fit(X_train, y_train)  
         
         coefficients = reg["estimator"].coef_
         bias = reg["estimator"].intercept_
-        features = reg["selector"].get_feature_names_out(relevant_columns)
+        features = reg["selector"].get_feature_names_out(feature_names)
         year_to_features[season] = features
         X_val = cv_dataset.loc[(cv_dataset["model"] == model)
-                               & (cv_dataset["year"] == season), relevant_columns].reset_index(drop=True)
+                               & (cv_dataset["year"] == season), feature_names].reset_index(drop=True)
         
         y_predicted = reg.predict(X_val)[0]
-        
-        national_forecasts_by_year.loc[national_forecasts_by_year["year"] == season, "predicted"] = y_predicted
+        #print(y_predicted)
+        #print(cv_dataset.loc[(cv_dataset["model"] == model)
+        #                       & (cv_dataset["year"] == season), "yield anomaly [%]"])
+        forecasts_by_year.loc[forecasts_by_year["year"] == season, "predicted"] = y_predicted
     
-    return (national_forecasts_by_year, year_to_features)
+    return (forecasts_by_year, year_to_features)
